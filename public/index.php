df433f171344d7e46880910fadde72cb103c1c4c
ChalleNgerOvO <1421724522@qq.com>
2024-05-15 14:12:26 +0800

    此时网站，各个路由应该能正常访问（项目1完成）

--- public/index.php
+++ public/index.php
@@ -2,11 +2,6 @@
 
 require '../helpers.php';
 
-
 $uri = $_SERVER['REQUEST_URI'];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    require(basePath($routes['404']));
-}
+require basePath('router.php');
