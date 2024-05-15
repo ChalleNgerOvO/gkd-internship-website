5e7bb4e661e255a7a74d7f1fef6775cae320817b
ChalleNgerOvO <1421724522@qq.com>
2024-05-15 15:00:02 +0800

    引入面向对象的方法重构路由处理逻辑

--- router.php
+++ router.php
@@ -1,10 +1,48 @@
 <?php
 
-$routes = require basePath('routes.php');
+class Router
+{
+    protected $routes = [];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    http_response_code(404);
-    require(basePath($routes['404']));
+    private function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+        ];
+    }
+
+    public function addGet($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    public function addPost($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    public function addPut($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    public function addDelete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        http_response_code(404);
+        loadView('error/404');
+    }
 }
