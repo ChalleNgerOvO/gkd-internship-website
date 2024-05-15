1a654c8001aa8d749eeaf65cc8637a0c4d47b5d6
ChalleNgerOvO <1421724522@qq.com>
2024-05-15 14:12:02 +0800

    刷新网站，各个路由应该能正常访问（项目1全部完成）

--- public/index.php
+++ public/index.php
@@ -2,13 +2,6 @@
 
 require '../helpers.php';
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 
 $uri = $_SERVER['REQUEST_URI'];
 
